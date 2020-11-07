bd2731172163bee0d5a5851c99489f7cea797c4d
REFRESH <refresh@johnny.local>
2020-11-07 16:24:16 +0300

    feat: desc refactor tryout

--- src/components/Description.js
+++ src/components/Description.js
@@ -4,15 +4,21 @@ import { initialContent as content } from '../utils/content';
 function Description() {
   return (
     <section className="description">
-			{ content.map((paragraph, key) => {
+			{ content.map((paragraph, number) => {
 				return paragraph &&
 				<>
-					<h2 className="description__title" key={key}>{paragraph.title ? paragraph.title : ''}</h2>
-					<p className="description__paragraph">{paragraph.text ? paragraph.text : ''}</p>
+					<h2 className="description__title" key={ number.toString() }>
+						{ paragraph.title ? paragraph.title : null }
+					</h2>
+
+					{ paragraph.text ? paragraph.text.map((item, number) => {
+						return <p className="description__paragraph" key={ number.toString() }>{ item }</p>
+					}) : null}
+
 					<ul className="description__list">
-								{paragraph.list ? paragraph.list.map((item, key) => {
-									return <li className="description__item" key={key}>{paragraph.list[key]}</li>
-								}) : ''}
+						{ paragraph.list ? paragraph.list.map((item, number) => {
+							return <li className="description__item" key={ number.toString() }>{ item }</li>
+						}) : null}
 					</ul>
 				</>
 			})
@@ -21,5 +27,6 @@ function Description() {
     </section>
   )
 }
+
 
 export default Description;
