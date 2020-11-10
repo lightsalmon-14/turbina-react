afe4aa8c28f3c559a977e6abf6db251373238134
Yulia Ivanova <yuliaiv.yulia@yandex.ru>
2020-11-10 21:50:55 +1000

    fixed some css

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ function Footer() {
   return (
     <footer className="footer">
       <p className="footer__copyright">&copy; Маршак, 2020.</p>
-      <p className="footer__copyright">Сделано студентами <a className="footer__link" target="_blank"
+      <p className="footer__develop">Сделано студентами <a className="footer__link" target="_blank"
         href="https://praktikum.yandex.ru">Яндекс.Практикум</a></p>
     </footer>
   )
